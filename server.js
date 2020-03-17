65006384566d940e4674159d7b5308ee6574e84e
chunzhengyang <47287076+chunzhengyang@users.noreply.github.com>
2020-03-17 03:28:32 -0700

    init

--- server.js
+++ server.js
@@ -3,17 +3,13 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 5000;
 
-app.use(express.static('./client/build'))
-
-app.get('*', (req, res) => {    
-    res.sendfile('./client/build/index.html')
-})
-
-// app.use(express.static(path.join(__dirname, 'client/public')));
+app.use(express.static('./client/build/index.html'))
 
 // app.get('*', (req, res) => {    
-//     res.sendfile(path.join(__dirname = 'client/public/index.html'));  
+//     res.sendfile('./client/build/index.html')
 // })
+
+// app.use(express.static(path.join(__dirname, 'client/public')));
 
 app.listen(port, (req, res) => {  
     console.log( `server listening on port: ${port}`)
