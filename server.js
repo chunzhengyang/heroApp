399d28300364d9054af17a3326e88da3966d55ec
chunzhengyang <47287076+chunzhengyang@users.noreply.github.com>
2020-03-17 03:42:04 -0700

    init

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 5000;
 
-app.use(express.static('client/build/index.html'))
+app.use(express.static(path.join(__dirname, 'client/build/index.html')))
 
 app.get('*', (req, res) => {    
-    res.sendfile('client/build/index.html')
+    res.sendfile(path.join(__dirname, 'client/build/index.html'))
 })
 
 // app.use(express.static(path.join(__dirname, 'client/public')));
