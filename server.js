5f1cfbb37a0f7434563aecd7efbcf7b7f1673b5f
chunzhengyang <47287076+chunzhengyang@users.noreply.github.com>
2020-03-17 03:07:43 -0700

    init

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 5000;
+
+console.log(__dirname)
 
 //production mode
-//if(process.env.NODE_ENV === 'production') {  
+if(process.env.NODE_ENV === 'production') {  
 
-    console.log('production mode')
+    console.log('Production mode')
 
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static('client/public'))
 
     app.get('*', (req, res) => {    
-        res.sendfile(path.join(__dirname = 'client/build/index.html'));  
-    })//}
+        res.sendfile('client/public/index.html')
+    })
+
+} else{
+
+    console.log('Development mode')
+
+    app.use(express.static(path.join(__dirname, 'client/public')));
+
+    app.get('*', (req, res) => {    
+        res.sendfile(path.join(__dirname = 'client/public/index.html'));  
+    })
+}
 
 //start server
 app.listen(port, (req, res) => {  
